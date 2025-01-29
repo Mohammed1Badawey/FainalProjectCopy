958ed69b44997c7d35c6c6df80d1d529d6a3b5ec
Mohammed1Badawey <MuhammadMohamed32@HotMail.com>
2025-01-29 20:28:16 +0200

    FinalNavbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import Logo from "../../assets/freshcart-logo.svg";
-import NavbarCss from "./Navbar.module.css"
+import NavbarCss from "./Navbar.module.css";
 import { Link, NavLink } from "react-router-dom";
 import HumburgerBotton from "../../assets/NavBar/burger-menu.svg";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [MenuOpenClass, setMenuOpenClass] = useState(
+    "p-0.5 border bg-slate-500 rounded text-gray-600 border-gray-400 hover:text-gray-800 border-gray-600"
+  );
+  const [MenuCloseClass, setMenuCloseClass] = useState(
+    "p-0.5 border rounded text-gray-600 border-gray-400 hover:text-gray-800 hover:border-gray-600"
+  );
 
   return (
     <>
       <div>
         <nav className="bg-slate-400 z-[9999] fixed inset-x-0 top-0 border-gray-200 min-h-18 mainNavbar">
-
           <div className="flex flex-wrap justify-between items-center max-w-screen-2xl p-4 mx-auto container">
-            
             {/* Logo */}
             <div className="flex items-center gap-5">
               <NavLink
@@ -49,10 +53,8 @@ export default function Navbar() {
               </ul>
             </div>
 
-
             {/* SocialMedia and Login for large Screens */}
             <div className=" items-center space-x-6 rtl:space-x-reverse hidden lg:flex">
-
               <ul className="flex gap-3">
                 <li>
                   <i className="fab fa-facebook"></i>
@@ -70,7 +72,7 @@ export default function Navbar() {
                   <i className="fab fa-youtube"></i>
                 </li>
               </ul>
-              
+
               <ul className="flex gap-3">
                 <li>
                   <Link to="login">Login</Link>
@@ -82,103 +84,93 @@ export default function Navbar() {
                   <Link to="">Signout</Link>
                 </li>
               </ul>
-
             </div>
 
             <div className="lg:hidden">
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)} 
-              className="p-0.5 border rounded text-gray-600 border-gray-400 hover:text-gray-800 hover:border-gray-600">
-              <figure>
-                <img src={HumburgerBotton} alt="MenuHumburgerBotton" />
-              </figure>
-            </button>
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className={isMenuOpen ? MenuOpenClass : MenuCloseClass}
+              >
+                <figure>
+                  <img src={HumburgerBotton} alt="MenuHumburgerBotton" />
+                </figure>
+              </button>
             </div>
-
           </div>
 
           <div>
             {isMenuOpen && (
-                <div className="lg:hidden bg-slate-400 mobileNavbar">
-
-
-
-
+              <div className="lg:hidden bg-slate-400 mobileNavbar">
                 <ul className="flex flex-col  items-center space-y-3 ">
-                <li className="">
-                  <NavLink to="" onClick={() => setIsMenuOpen(false)}>
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="cart" onClick={() => setIsMenuOpen(false)}>
-                    Cart
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="products" onClick={() => setIsMenuOpen(false)}>
-                    Products
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="categories" onClick={() => setIsMenuOpen(false)}>
-                    Categories
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="brands" onClick={() => setIsMenuOpen(false)}>
-                    Brands
-                  </NavLink>
-                </li>
-                <li>
-                  <Link to="login" onClick={() => setIsMenuOpen(false)}>
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link to="register" onClick={() => setIsMenuOpen(false)}>
-                    Register
-                  </Link>
-                </li>
-                <li>
-                  <Link to="" onClick={() => setIsMenuOpen(false)}>
-                    Signout
-                  </Link>
+                  <li className="">
+                    <NavLink to="" onClick={() => setIsMenuOpen(false)}>
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="cart" onClick={() => setIsMenuOpen(false)}>
+                      Cart
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="products" onClick={() => setIsMenuOpen(false)}>
+                      Products
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="categories"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Categories
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="brands" onClick={() => setIsMenuOpen(false)}>
+                      Brands
+                    </NavLink>
+                  </li>
+                  <li>
+                    <Link to="login" onClick={() => setIsMenuOpen(false)}>
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="register" onClick={() => setIsMenuOpen(false)}>
+                      Register
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="" onClick={() => setIsMenuOpen(false)}>
+                      Signout
+                    </Link>
+                  </li>
+                </ul>
 
-                </li>
-              </ul>
-
-              <div className="socialMobileNavbar mx-auto py-3 bg-slate-500 mt-2">               
-              
-              <ul className="flex justify-center gap-3">
-                <li>
-                  <i className="fab fa-facebook"></i>
-                </li>
-                <li>
-                  <i className="fab fa-twitter"></i>
-                </li>
-                <li>
-                  <i className="fab fa-linkedin"></i>
-                </li>
-                <li>
-                  <i className="fab fa-tiktok"></i>
-                </li>
-                <li>
-                  <i className="fab fa-youtube"></i>
-                </li>
-              </ul> 
-              
-              </div>
+                <div className="socialMobileNavbar mx-auto bg-slate-500 mt-2 min-h-18">
+                  <ul className="flex justify-center min-h-18 items-center gap-3">
+                    <li>
+                      <i className="fab fa-facebook"></i>
+                    </li>
+                    <li>
+                      <i className="fab fa-twitter"></i>
+                    </li>
+                    <li>
+                      <i className="fab fa-linkedin"></i>
+                    </li>
+                    <li>
+                      <i className="fab fa-tiktok"></i>
+                    </li>
+                    <li>
+                      <i className="fab fa-youtube"></i>
+                    </li>
+                  </ul>
                 </div>
-              )}
-
-  
-            </div>
-
-
+              </div>
+            )}
+          </div>
         </nav>
       </div>
-
     </>
   );
 }
