213ce43f55e5aaec680a2fa605223c998c1ef2f7
Mohammed1Badawey <MuhammadMohamed32@hotmail.com>
2025-01-30 09:25:04 +0200

    editNavbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,15 +3,61 @@ import Logo from "../../assets/freshcart-logo.svg";
 import NavbarCss from "./Navbar.module.css";
 import { Link, NavLink } from "react-router-dom";
 import HumburgerBotton from "../../assets/NavBar/burger-menu.svg";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
+import classNames from "classnames";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [MenuOpenClass, setMenuOpenClass] = useState(
-    "p-0.5 border bg-slate-500 rounded text-gray-600 border-gray-400 hover:text-gray-800 border-gray-600",
+
+  const menuClass = classNames(
+    "p-0.5",
+    "border",
+    "rounded",
+    "text-gray-600",
+    "border-gray-500",
+    "hover:text-gray-800",
+    { "bg-slate-500": isMenuOpen },
   );
-  const [MenuCloseClass, setMenuCloseClass] = useState(
-    "p-0.5 border rounded text-gray-600 border-gray-400 hover:text-gray-800 hover:border-gray-600",
-  );
+
+  const navLinks = [
+    { name: "Home", path: "" },
+    { name: "Cart", path: "cart" },
+    { name: "Products", path: "products" },
+    { name: "Categories", path: "categories" },
+    { name: "Brands", path: "brands" },
+  ];
+
+  const authLinks = [
+    { name: "Login", path: "login" },
+    { name: "Register", path: "register" },
+    { name: "Signout", path: "" },
+  ];
+
+  const iconClass = "text-xl text-gray-900 hover:text-gray-950";
+
+  const socialIcons = {
+    facebook: (
+      <FaFacebook className={iconClass} />
+    ),
+    twitter: (
+      <FaTwitter className={iconClass} />
+    ),
+    linkedin: (
+      <FaLinkedin className={iconClass} />
+    ),
+    tiktok: <FaTiktok className={iconClass} />,
+    youtube: (
+      <FaYoutube className={iconClass} />
+    ),
+  };
+
+  const socialLinks = ["facebook", "twitter", "linkedin", "tiktok", "youtube"];
 
   return (
     <>
@@ -30,66 +76,35 @@ export default function Navbar() {
               {/* pages for lg Screen and higher */}
 
               <ul className="hidden gap-3 text-slate-800 lg:flex">
-                <li>
-                  {" "}
-                  <NavLink to="">Home</NavLink>{" "}
-                </li>
-                <li>
-                  {" "}
-                  <NavLink to="cart">Cart</NavLink>{" "}
-                </li>
-                <li>
-                  {" "}
-                  <NavLink to="products">Products</NavLink>{" "}
-                </li>
-                <li>
-                  {" "}
-                  <NavLink to="categories">Categories</NavLink>{" "}
-                </li>
-                <li className="me-3">
-                  {" "}
-                  <NavLink to="brands">Brands</NavLink>{" "}
-                </li>
+                {navLinks.map((link) => (
+                  <li key={link.name}>
+                    <NavLink to={link.path}>{link.name}</NavLink>
+                  </li>
+                ))}
               </ul>
             </div>
 
             {/* SocialMedia and Login for large Screens */}
             <div className="hidden items-center space-x-6 lg:flex rtl:space-x-reverse">
               <ul className="flex gap-3">
-                <li>
-                  <i className="fab fa-facebook"></i>
-                </li>
-                <li>
-                  <i className="fab fa-twitter"></i>
-                </li>
-                <li>
-                  <i className="fab fa-linkedin"></i>
-                </li>
-                <li>
-                  <i className="fab fa-tiktok"></i>
-                </li>
-                <li>
-                  <i className="fab fa-youtube"></i>
-                </li>
+                {socialLinks.map((platform) => (
+                  <li key={platform}>{socialIcons[platform]}</li>
+                ))}
               </ul>
 
-              <ul className="flex gap-3">
-                <li>
-                  <Link to="login">Login</Link>
-                </li>
-                <li>
-                  <Link to="register">Register</Link>
-                </li>
-                <li>
-                  <Link to="">Signout</Link>
-                </li>
+              <ul className="hidden gap-3 lg:flex">
+                {authLinks.map((link) => (
+                  <li key={link.name}>
+                    <Link to={link.path}>{link.name}</Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
             <div className="lg:hidden">
               <button
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
-                className={isMenuOpen ? MenuOpenClass : MenuCloseClass}
+                className={menuClass}
               >
                 <figure>
                   <img src={HumburgerBotton} alt="MenuHumburgerBotton" />
@@ -101,69 +116,27 @@ export default function Navbar() {
           <div>
             {isMenuOpen && (
               <div className="mobileNavbar bg-slate-400 lg:hidden">
-                <ul className="flex flex-col items-center space-y-3">
-                  <li className="">
-                    <NavLink to="" onClick={() => setIsMenuOpen(false)}>
-                      Home
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="cart" onClick={() => setIsMenuOpen(false)}>
-                      Cart
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="products" onClick={() => setIsMenuOpen(false)}>
-                      Products
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="categories"
-                      onClick={() => setIsMenuOpen(false)}
-                    >
-                      Categories
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="brands" onClick={() => setIsMenuOpen(false)}>
-                      Brands
-                    </NavLink>
-                  </li>
-                  <li>
-                    <Link to="login" onClick={() => setIsMenuOpen(false)}>
-                      Login
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="register" onClick={() => setIsMenuOpen(false)}>
-                      Register
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="" onClick={() => setIsMenuOpen(false)}>
-                      Signout
-                    </Link>
-                  </li>
+                <ul
+                  className="flex flex-col items-center space-y-3"
+                >
+                  {navLinks.map((link) => (
+                    <li key={link.name}>
+                      <NavLink to={link.path} onClick={() => setIsMenuOpen(false)}>{link.name}</NavLink>
+                    </li>
+                  ))}
+
+                  {authLinks.map((link) => (
+                    <li key={link.name}>
+                      <Link to={link.path} onClick={() => setIsMenuOpen(false)}>{link.name}</Link>
+                    </li>
+                  ))}
                 </ul>
 
                 <div className="socialMobileNavbar mx-auto mt-2 min-h-18 bg-slate-500">
-                  <ul className="flex min-h-18 items-center justify-center gap-3">
-                    <li>
-                      <i className="fab fa-facebook"></i>
-                    </li>
-                    <li>
-                      <i className="fab fa-twitter"></i>
-                    </li>
-                    <li>
-                      <i className="fab fa-linkedin"></i>
-                    </li>
-                    <li>
-                      <i className="fab fa-tiktok"></i>
-                    </li>
-                    <li>
-                      <i className="fab fa-youtube"></i>
-                    </li>
+                  <ul className="flex min-h-18 items-center justify-center gap-3" >
+                    {socialLinks.map((platform) => (
+                      <li key={platform} onClick={() => setIsMenuOpen(false)}>{socialIcons[platform]}</li>
+                    ))}
                   </ul>
                 </div>
               </div>
